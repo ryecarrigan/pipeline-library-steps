0bfebf060345b262c4aecc23834a5bcfa1f7ac89
Rye Carrigan <rcarrigan@cloudbees.com>
2020-08-27 16:32:11 -0400

    remove yaml and cloud

--- vars/buildFlow.groovy
+++ vars/buildFlow.groovy
@@ -1,26 +1,7 @@
 void call(Closure body) {
     String label = randomPodLabel()
     stage('Linting Step') {
-        def yaml = """
-apiVersion: v1
-kind: Pod
-metadata:
-  labels:
-    some-label: some-label-value
-spec:
-  containers:
-  - name: busybox
-    image: busybox
-    command:
-    - cat
-    tty: true
-"""
-        def cloud_name = 'kubernetes'
-        podTemplate(
-            label: label,
-            yaml: yaml,
-            cloud: cloud_name,
-        ) {
+        podTemplate(label: label) {
             node(label) {
                 echo("Bonjour")
             }
